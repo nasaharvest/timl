fe0023ad745161b7c01650c851f0476f7ede4322
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2022-02-15 07:26:41 -0500

    Add jit-saving to the crop classifier, with tests

---
+++ crop_classification/tests/test_lstm.py
@@ -0,0 +1,57 @@
+import torch
+import numpy as np
+from pathlib import Path
+
+from dl.lstm import Classifier
+from dl.encoder import TaskEncoder
+
+
+def test_jit(tmp_path):
+
+    input_size, num_timesteps, input_task_info_size = 18, 12, 13
+
+    # train a model
+    model = Classifier(
+        input_size=input_size,
+        classifier_base_layers=1,
+        num_classification_layers=2,
+        classifier_dropout=0.2,
+        classifier_vector_size=128,
+    )
+    model.load_state_dict(
+        torch.load(Path(__file__).parent / "test_data/timl/state_dict.pth")
+    )
+
+    model.eval()
+
+    encoder = TaskEncoder(
+        input_size=input_task_info_size,
+        encoder_vector_sizes=[32, 64, 128],
+        encoder_dropout=0.2,
+        num_bands=input_size,
+        num_hidden_layers=2,
+        hidden_vector_size=128,
+        num_timesteps=num_timesteps,
+    )
+    encoder.load_state_dict(
+        torch.load(Path(__file__).parent / "test_data/timl/encoder_state_dict.pth")
+    )
+
+    encoder.eval()
+
+    task_info = torch.rand(input_task_info_size)
+
+    model.update_embeddings(encoder(task_info))
+
+    model.save("timl", tmp_path)
+
+    loaded_model = torch.jit.load(tmp_path / f"timl.pt")
+    loaded_model.eval()
+
+    x = torch.rand(5, num_timesteps, input_size)
+
+    with torch.no_grad():
+        loaded_y = loaded_model(x).numpy()
+        org_y = model(x).numpy()
+
+    assert np.allclose(loaded_y, org_y)
