90c958f4939ab9f3801ae34fffd2a7505c151f3f
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-02-28 12:03:58 -0500

    Move magic numbers to model config

--- crop_classification/dl/config.py
+++ crop_classification/dl/config.py
@@ -8,3 +8,12 @@ TEST_CROP_TO_CLASSIFICATION: Dict[str, CropClassifications] = {
     "coffee": CropClassifications.beverage_spice,
     "almond": CropClassifications.fruits_nuts,
 }
+
+# LSTM model configurations
+HIDDEN_VECTOR_SIZE = 128
+NUM_CLASSIFICATION_LAYERS = 2
+CLASSIFIER_DROPOUT = 0.2
+CLASSIFIER_BASE_LAYERS = 1
+
+ENCODER_VECTOR_SIZES = [32, 64, 128]
+ENCODER_DROPOUT = 0.2
